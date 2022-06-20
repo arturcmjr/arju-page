80993a12000a8440040eeb2f1878a40f53268ed5
Artur Junior <arturcmjr@gmail.com>
2022-06-20 16:11:30 -0300

    Too lazy to write descriptive message

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { ThemeProvider, createTheme } from "@mui/material";
+import { themeOptions } from "./shared/theme/muiTheme";
+import { Button } from "@mui/material";
+
+const theme = createTheme(themeOptions);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <h1>test malaka!</h1>
+      <p style={{fontFamily: 'Inconsolata'}}>04. bom dia Brasil</p>
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+      {/* <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div> */}
+    </ThemeProvider>
   );
 }
 
---
+++ src/shared/theme/muiTheme.ts
@@ -0,0 +1,19 @@
+import { ThemeOptions } from "@mui/material";
+
+export const themeOptions: ThemeOptions = {
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#4ecca3',
+    },
+    secondary: {
+      main: '#eeeeee',
+    },
+    background: {
+      paper: '#393e46',
+      default: '#232931',
+    },
+  },
+};
+
+export default themeOptions;
