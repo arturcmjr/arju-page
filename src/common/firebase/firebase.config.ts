9092929bb08f2b37a7216bce6407b15640e09b9e
Artur Junior <arturcmjr@gmail.com>
2022-11-08 18:15:18 +0000

    fixing deploy

---
+++ src/common/firebase/firebase.config.ts
@@ -0,0 +1,11 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAy6ev4L4hi_EB-MW8Bq21EIXMM4aKtggQ",
+  authDomain: "arju-dev.firebaseapp.com",
+  projectId: "arju-dev",
+  storageBucket: "arju-dev.appspot.com",
+  messagingSenderId: "347374461077",
+  appId: "1:347374461077:web:8b3983528ce238fc7c7b9c",
+  measurementId: "G-204DZYD1PB"
+};
+
+export default firebaseConfig;
